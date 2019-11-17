18c486d7682aa389c4b294adecae813164da8b42
laierkasten23 <lia.schmid98@gmx.de>
2019-11-17 20:51:42 +0000

    E4

--- Ex4/Ex4.R
+++ Ex4/Ex4.R
@@ -17,7 +17,7 @@ epsil = 0.000001 # relative convergence criterion as a stop rule
 
 # Likelihood function
 lik <- function(lambda){
-  lambda^n * e^(-lambda*sum(x))
+  lambda^n * exp(-lambda * sum(x))
 }
 
 # loglikelihood function
@@ -27,7 +27,7 @@ loglik <- function(lambda){
 
 # score function
 s <- function(lambda){
-  n/lambda + sum(x)
+  n/lambda - sum(x)
 }
 
 # score derivative
@@ -35,23 +35,38 @@ s.prime <- function(lambda){
   -n/lambda^2
 }
 
+par(mfrow=c(1,3))
+lambda <- seq(0.1,0.9,0.05)
+plot(lambda,lik(lambda),ylab="likelihood",
+     xlab=expression(lambda),lwd=2,type="l")
+box(lwd=2)
+plot(lambda,loglik(lambda),ylab="log-likelihood",
+     xlab=expression(lambda),lwd=2,type="l")
+box(lwd=2)
+plot(lambda,s(lambda),ylab="score",
+     xlab=expression(lambda),
+     lwd=2,type="l")
+abline(h=0,lty=3); box(lwd=2)
+
+
+
 # (a) Use the observed data and the method of Newton-Raphson to approximate the ML 
 # estimate of ??. Justify your choice of the initial estimate and all the intermediate steps.
 
 NR      <- function(lambda0,eps){
   # x      : observed sample
-  # [lambda0: initial estimate of lambda
+  # lambda0: initial estimate of lambda
   # eps    : is the stopping rule
   
   # the NR method iterates until the stopping criterion is validated
   # we will use as stopping rule diff = |lambda_(t+1)-lambda_(t)| <= eps
   lambda.it    = vector()
-  lambda.it[1] = alpha0
+  lambda.it[1] = lambda0
   k           = 1
   diff        = 1
   while(diff>eps){
     lambda.it[k+1] = lambda.it[k]-s(lambda.it[k])/s.prime(lambda.it[k])
-    diff          = abs(alpha.it[k+1]-alpha.it[k])
+    diff          = abs(lambda.it[k+1]-lambda.it[k])
     k             = k+1
   }
   result = as.matrix(lambda.it)
@@ -60,7 +75,37 @@ NR      <- function(lambda0,eps){
   result
 }
 
+t(NR(2,epsil))
+
 # (b) Consider the following reparametrization b = log(??) in the pdf above. Implement the 
 # Newton-Rapshon algorithm to approximate the ML estimate of ?? using this reparametrization
+
+# score function
+s <- function(b){n-exp(b)*sum(x)}
+
+# derivative of the (pro???le) score function 
+s.prime <- function(b){-exp(b)*sum(x)}
+
+
+NR <- function(x,lambda0,eps){ 
+  # x : observed sample 
+  # lambda0 : initial estimate of lambda
+  # eps : is the stopping rule
+  n = length(x); b0 = log(lambda0)
+  b.it = vector(); b.it[1] = b0
+  lambda.it = vector(); lambda.it[1] = lambda0 
+  k = 1; diff = 1 
+  while(diff>eps){ 
+    b.it[k+1] = b.it[k]-s(b.it[k])/s.prime(b.it[k]) 
+    lambda.it[k+1] = exp(b.it[k+1]) 
+    diff = abs(b.it[k+1]-b.it[k])
+    k = k+1 
+    } 
+  result = as.matrix(cbind(b.it,lambda.it)) 
+  rownames(result)<-1:length(lambda.it);
+  result
+}
+
+
 
 # (c) Which approach, (a) or (b) is more sensitive to the initial values?
